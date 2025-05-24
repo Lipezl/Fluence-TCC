0db17a0e56e543e8fc85a9f2afb3553460861d76
felipe <felipemantuani983@gmail.com>
2025-05-24 12:22:05 -0300

    Initializing the project

---
+++ app.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import routes from './routes/index.js';
+
+const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+
+export default app;

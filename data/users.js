3116d4541e4d995411c3a18c1f4d2aed538adfa2
Lipezl <felipemantuani4@gmail.com>
2025-06-30 23:55:22 -0300

    teste

--- data/users.js
+++ data/users.js
@@ -1,10 +1,11 @@
 export const users = [
     {
+        //Teste de usuário
         id: 1,
         nome: 'Milena / amor da minha vida',
         senha: 'euAmoEla',
         email: 'milena@gmail.com',
         escolaridade: 'Ensino Médio',
         dataNascimento: '2008-12-29'
-    }
+    } 
 ];

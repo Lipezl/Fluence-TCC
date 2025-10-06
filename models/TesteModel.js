f103e259c06dea082ec446e4353bf2ff2f41817b
Lipezl <felipemantuani4@gmail.com>
2025-10-05 22:52:50 -0300

    update

--- models/TesteModel.js
+++ models/TesteModel.js
@@ -52,29 +52,37 @@ function buscarDetalhesTeste(testeId, callback) {
 }
 
 export function sortearTextoParaUsuario(userId, callback) {
-    // Busca os textos que o usuário já realizou
-    const queryFeitos = `
-        SELECT textos_leitura_id FROM testes WHERE usuario_id = ?
-    `;
-    db.query(queryFeitos, [userId], (err, feitos) => {
+    db.query('SELECT nivel FROM usuarios WHERE id = ?', [userId], (err, results) => {
         if (err) return callback(err);
+        if (!results.length) return callback(new Error('Usuário não encontrado'));
 
-        const feitosIds = feitos.map(f => f.textos_leitura_id);
+        const nivelUsuario = results[0].nivel;
 
-        let queryDisponiveis = 'SELECT * FROM textos_leitura';
-        if (feitosIds.length > 0) {
-            queryDisponiveis += ` WHERE id NOT IN (${feitosIds.join(',')})`;
-        }
-
-        db.query(queryDisponiveis, (err, textosDisponiveis) => {
+        const queryFeitos = `
+            SELECT textos_leitura_id FROM testes WHERE usuario_id = ?
+        `;
+        db.query(queryFeitos, [userId], (err, feitos) => {
             if (err) return callback(err);
 
-            if (textosDisponiveis.length === 0) {
-                return callback(null, null);
+            const feitosIds = feitos.map(f => f.textos_leitura_id);
+
+            let queryDisponiveis = 'SELECT * FROM textos_leitura WHERE nivel <= ?';
+            let params = [nivelUsuario];
+
+            if (feitosIds.length > 0) {
+                queryDisponiveis += ` AND id NOT IN (${feitosIds.join(',')})`;
             }
 
-            const textoSorteado = textosDisponiveis[Math.floor(Math.random() * textosDisponiveis.length)];
-            callback(null, textoSorteado);
+            db.query(queryDisponiveis, params, (err, textosDisponiveis) => {
+                if (err) return callback(err);
+
+                if (textosDisponiveis.length === 0) {
+                    return callback(null, null);
+                }
+
+                const textoSorteado = textosDisponiveis[Math.floor(Math.random() * textosDisponiveis.length)];
+                callback(null, textoSorteado);
+            });
         });
     });
 }

bc5767255ec05136653bcd015673432d587ab839
Lipezl <felipemantuani4@gmail.com>
2025-11-29 22:33:57 -0300

    Update

--- DumpDB/fluence_testes.sql
+++ DumpDB/fluence_testes.sql
@@ -18,7 +18,6 @@
 --
 -- Table structure for table `testes`
 --
-
 DROP TABLE IF EXISTS `testes`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
